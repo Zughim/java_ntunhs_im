70307da94931f0a11785084fb74dbf0579920886
Zughim <160557154+Zughim@users.noreply.github.com>
2024-02-27 11:56:56 +0800

    Add files via upload

---
+++ hw1.java
@@ -0,0 +1,21 @@
+package hw1; 
+
+public class hw1 {
+	static int pv;
+	static int fv;
+	static final double interest=0.05;
+	static int interval;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		pv=500000;
+		interval=12;
+		
+		fv=(int) (pv*Math.pow((1+interest),interval));
+		
+		System.out.println("My pv is"+pv+"and after"+interval+"year it would be"+fv);
+	}
+
+}
